3bb23c6186eb0b96b89b88a100171424d8a978bb
TondaNosov <nosov.anton.cz@gmail.com>
2022-07-09 12:40:53 +0200

    Add map

--- js/index.js
+++ js/index.js
@@ -27,3 +27,25 @@ galerySlider.addEventListener("input", function() {
   galeryImageAfter.style.width =  `${galerySlider.value}%`;
   galeryImageBefore.style.width = 100 - `${galerySlider.value}` + "%";
 });
+
+/** map */
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    mapId: "91f011b3a5d9f2e",
+    center: { lat: 50.075389063382566, lng: 14.436260266094148 },
+    zoom: 15,
+  });
+
+  new google.maps.Marker({
+    position: { lat: 50.075389063382566, lng: 14.436260266094148 },
+    map,
+    title: "Cat Energy",
+    icon: {
+      url: "../img/map-pin.png",
+      scaledSize: new google.maps.Size(56, 53),
+    }
+  });
+}
+
+window.initMap = initMap;
